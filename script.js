48bfaba906edff74ca83d66b3ee6aeccb7ba3f1a
brad hendrickse <bradh@box.org.za>
2022-01-24 16:42:06 +0200

    added event listeners to the clear and number buttons and update the display

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ function operate(operator, number1, number2) {
       break;
   }
 }
+
+let display = document.querySelector('.display');
+let buttonClear = document.querySelector('.buttonClear');
+let numberButton = document.querySelectorAll('.numberButton');
+let buttonEquals = document.querySelector('.buttonEquals');
+let buttonDivide = document.querySelector('.buttonDivide');
+let buttonMultiply = document.querySelector('.buttonMultiply');
+let buttonSubtract = document.querySelector('.buttonSubtract');
+let buttonAdd = document.querySelector('.buttonAdd');
+let buttonPoint = document.querySelector('.buttonPoint');
+
+function addNumberToDisplay(number) {
+  const currentDisplayValue = display.value;
+  console.log(typeof currentDisplayValue);
+  if (currentDisplayValue === '0') {
+    display.value = number;
+  } else {
+    display.value = currentDisplayValue + '' + number;
+  }
+}
+
+buttonClear.addEventListener('click', (e) => {
+  display.value = '0';
+});
+
+numberButton.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    addNumberToDisplay(e.target.textContent);
+    console.log(e.target.textContent);
+  });
+});
