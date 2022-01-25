6220ca12021437d32dd21c64a08c18a6997e76b3
brad hendrickse <bradh@box.org.za>
2022-01-25 14:29:18 +0200

    cleaned up a little; added the ability to use floating point numbers

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
+function addNumberToDisplay(number) {
+  const currentDisplayValue = display.value;
+  console.log(typeof currentDisplayValue);
+  if (currentDisplayValue === '0') {
+    if (number === '.') {
+      display.value = currentDisplayValue + '' + number;
+    } else {
+      display.value = number;
+    }
+  } else {
+    if (number === '.') {
+      if (currentDisplayValue.indexOf('.') === -1) {
+        display.value = currentDisplayValue + '' + number;
+      }
+    } else {
+      display.value = currentDisplayValue + '' + number;
+    }
+  }
+}
+
 function operate(operator, number1, number2) {
   switch (operator) {
     case '+':
@@ -19,26 +39,8 @@ function operate(operator, number1, number2) {
 
 let display = document.querySelector('.display');
 let buttonClear = document.querySelector('.buttonClear');
-
 let numberButtons = document.querySelectorAll('.numberButton');
 let operatorButtons = document.querySelectorAll('.operatorButton');
-
-let buttonEquals = document.querySelector('.buttonEquals');
-let buttonDivide = document.querySelector('.buttonDivide');
-let buttonMultiply = document.querySelector('.buttonMultiply');
-let buttonSubtract = document.querySelector('.buttonSubtract');
-let buttonAdd = document.querySelector('.buttonAdd');
-let buttonPoint = document.querySelector('.buttonPoint');
-
-function addNumberToDisplay(number) {
-  const currentDisplayValue = display.value;
-  console.log(typeof currentDisplayValue);
-  if (currentDisplayValue === '0') {
-    display.value = number;
-  } else {
-    display.value = currentDisplayValue + '' + number;
-  }
-}
 
 buttonClear.addEventListener('click', (e) => {
   display.value = '0';
